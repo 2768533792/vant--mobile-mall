d140351a6b9fb0bdc6835413dce8f712d3dc4817
钱招严 <qzy@mamaqunaer.com>
2018-01-24 11:26:55 +0800

    增加时间格式化的全局 filter

--- src/vue/filter/index.js
+++ src/vue/filter/index.js
@@ -1,3 +1,10 @@
+import moment from 'moment';
+moment.locale('zh-cn');
+
+export const dateFormat = (value, format = 'YYYY-MM-DD') => {
+	return value ? moment(value * 1000).format(format) : '';
+}
+
 export const yuan = (value) => {
 	return !isNaN(value) ? '¥' + (value / 100).toFixed(2) : value;
 }
@@ -5,5 +12,6 @@ export const yuan = (value) => {
 export default {
 	install(Vue) {
 		Vue.filter('yuan', yuan)
+		Vue.filter('dateFormat', dateFormat)
 	},
 }
